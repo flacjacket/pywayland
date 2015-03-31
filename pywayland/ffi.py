ee31cc24a3d2d5509df6050664e7afbe4762f63f
Sean Vig <sean.v.775@gmail.com>
2015-03-30 19:45:39 -0500

    Add cffi calls for client
    Add functions and definitions necessary for using client-side objects and
    protocols.

--- pywayland/ffi.py
+++ pywayland/ffi.py
@@ -50,6 +50,37 @@ union wl_argument {
 typedef int (*wl_dispatcher_func_t)(const void *, void *, uint32_t,
                                     const struct wl_message *,
                                     union wl_argument *);
+
+/******************************************************************************
+ * wayland-client.h
+ *****************************************************************************/
+// wl_display methods
+struct wl_display* wl_display_connect(const char *name);
+struct wl_display* wl_display_connect_to_fd(int fd);
+struct wl_event_queue *wl_display_create_queue(struct wl_display *display);
+void wl_display_disconnect(struct wl_display *display);
+int wl_display_dispatch(struct wl_display *display);
+int wl_display_flush(struct wl_display *display);
+int wl_display_get_fd(struct wl_display *display);
+int wl_display_roundtrip(struct wl_display *display);
+
+// wl_eventqueue methods
+void wl_event_queue_destroy(struct wl_event_queue *queue);
+
+// wl_proxy methods
+struct wl_proxy *wl_proxy_create(struct wl_proxy *factory,
+                                 const struct wl_interface *interface);
+void wl_proxy_destroy(struct wl_proxy *proxy);
+int wl_proxy_add_dispatcher(struct wl_proxy *proxy,
+                            wl_dispatcher_func_t dispatcher_func,
+                            const void * dispatcher_data, void *data);
+void *wl_proxy_get_user_data(struct wl_proxy *proxy);
+void wl_proxy_marshal_array(struct wl_proxy *p, uint32_t opcode,
+                            union wl_argument *args);
+struct wl_proxy * wl_proxy_marshal_array_constructor(struct wl_proxy *proxy,
+                                                     uint32_t opcode, union wl_argument *args,
+                                                     const struct wl_interface *interface);
+void wl_proxy_set_user_data(struct wl_proxy *proxy, void *user_data);
 """)
 
 C = ffi.verify("""
