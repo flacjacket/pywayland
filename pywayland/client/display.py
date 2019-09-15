9e1a0797fd73124d77fa12310d75c6c279b2379f
Sean Vig <sean.v.775@gmail.com>
2019-09-14 23:52:25 -0400

    Consolidate the client display methods
    Move the variations in the client display methods to instead be behind
    single methods, with the different behaviors controlled by more
    understandable kwargs.

--- pywayland/client/display.py
+++ pywayland/client/display.py
@@ -22,58 +22,57 @@ from weakref import WeakSet
 
 
 class Display(WlDisplay.proxy_class):  # type: ignore
-    """Represents a connection to the compositor
+    def __init__(self, name_or_fd: Union[int, str] = None) -> None:
+        """Represents a connection to the compositor
 
-    A :class:`Display` object represents a client connection to a Wayland
-    compositor.  The connection and the corresponding Wayland object are
-    created with :func:`Display.connect`.  The display must be connected before
-    it can be used.  A connection is terminated using
-    :func:`Display.disconnect`.
+        A :class:`Display` object represents a client connection to a Wayland
+        compositor.  The connection and the corresponding Wayland object are
+        created with :func:`Display.connect`.  The display must be connected
+        before it can be used.  A connection is terminated using
+        :func:`Display.disconnect`.
 
-    A :class:`Display` is also used as the
-    :class:`~pywayland.client.proxy.Proxy` for the
-    :class:`pywayland.protocol.wayland.WlDisplay` protocol object on the
-    compositor side.
+        A :class:`Display` is also used as the
+        :class:`~pywayland.client.proxy.Proxy` for the
+        :class:`pywayland.protocol.wayland.WlDisplay` protocol object on the
+        compositor side.
 
-    A :class:`Display` object handles all the data sent from and to the
-    compositor.  When a :class:`~pywayland.client.proxy.Proxy` marshals a
-    request, it will write its wire representation to the display's write
-    buffer. The data is sent to the compositor when the client calls
-    :func:`flush`.
+        A :class:`Display` object handles all the data sent from and to the
+        compositor.  When a :class:`~pywayland.client.proxy.Proxy` marshals a
+        request, it will write its wire representation to the display's write
+        buffer. The data is sent to the compositor when the client calls
+        :func:`flush`.
 
-    Incoming data is handled in two steps: queueing and dispatching. In the
-    queue step, the data coming from the display fd is interpreted and added to
-    a queue. On the dispatch step, the handler for the incoming event set by
-    the client on the corresponding :class:`~pywayland.client.proxy.Proxy` is
-    called.
+        Incoming data is handled in two steps: queueing and dispatching. In the
+        queue step, the data coming from the display fd is interpreted and
+        added to a queue. On the dispatch step, the handler for the incoming
+        event set by the client on the corresponding
+        :class:`~pywayland.client.proxy.Proxy` is called.
 
-    A :class:`Display` has at least one event queue, called the `default
-    queue`.  Clients can create additional event queues with
-    :func:`Display.create_queue` and assign
-    :class:`~pywayland.client.proxy.Proxy`'s to it. Events occurring in a
-    particular proxy are always queued in its assigned queue.  A client can
-    ensure that a certain assumption, such as holding a lock or running from a
-    given thread, is true when a proxy event handler is called by assigning
-    that proxy to an event queue and making sure that this queue is only
-    dispatched when the assumption holds.
+        A :class:`Display` has at least one event queue, called the `default
+        queue`.  Clients can create additional event queues with
+        :func:`Display.create_queue` and assign
+        :class:`~pywayland.client.proxy.Proxy`'s to it. Events occurring in a
+        particular proxy are always queued in its assigned queue.  A client can
+        ensure that a certain assumption, such as holding a lock or running
+        from a given thread, is true when a proxy event handler is called by
+        assigning that proxy to an event queue and making sure that this queue
+        is only dispatched when the assumption holds.
 
-    The default queue is dispatched by calling :func:`Display.dispatch`.  This
-    will dispatch any events queued on the default queue and attempt to read
-    from the display fd if it's empty. Events read are then queued on the
-    appropriate queues according to the proxy assignment.
+        The default queue is dispatched by calling :func:`Display.dispatch`.
+        This will dispatch any events queued on the default queue and attempt
+        to read from the display fd if it's empty. Events read are then queued
+        on the appropriate queues according to the proxy assignment.
 
-    A user created queue is dispatched with :func:`Display.dispatch_queue`.
-    This function behaves exactly the same as :func:`Display.dispatch` but it
-    dispatches given queue instead of the default queue.
+        A user created queue is dispatched with :func:`Display.dispatch_queue`.
+        This function behaves exactly the same as :func:`Display.dispatch` but
+        it dispatches given queue instead of the default queue.
 
-    A real world example of event queue usage is Mesa's implementation of
-    glSwapBuffers() for the Wayland platform. This function might need to block
-    until a frame callback is received, but dispatching the default ueue could
-    cause an event handler on the client to start drawing gain.  This problem
-    is solved using another event queue, so that only the events handled by the
-    EGL code are dispatched during the block.
-    """
-    def __init__(self, name_or_fd: Union[int, str, None] = None) -> None:
+        A real world example of event queue usage is Mesa's implementation of
+        glSwapBuffers() for the Wayland platform. This function might need to
+        block until a frame callback is received, but dispatching the default
+        ueue could cause an event handler on the client to start drawing gain.
+        This problem is solved using another event queue, so that only the
+        events handled by the EGL code are dispatched during the block.  """
         # Initially, we have no pointer
         super().__init__(None)
 
@@ -81,10 +80,12 @@ class Display(WlDisplay.proxy_class):  # type: ignore
         self._name_or_fd = name_or_fd
 
     def __enter__(self) -> "Display":
+        """Connect to the display in a context manager"""
         self.connect()
         return self
 
     def __exit__(self, exc_type, exc_value, traceback) -> None:
+        """Disconnect from the display"""
         self.disconnect()
 
     def connect(self) -> None:
@@ -140,105 +141,65 @@ class Display(WlDisplay.proxy_class):  # type: ignore
         return lib.wl_display_get_fd(self._ptr)
 
     @ensure_valid
-    def dispatch(self) -> int:
+    def dispatch(self, *, block=False, queue: EventQueue = None) -> int:
         """Process incoming events
 
-        Dispatch the display's default event queue.
+        If block is `False`, it does not attempt to read the display fd or
+        event queue and simply returns zero if the queue is empty.
 
-        If the default event queue is empty, this function blocks until there
-        are events to be read from the display fd. Events are read and queued
-        on the appropriate event queues. Finally, events on the default event
-        queue are dispatched.
+        If the given queue is empty and `block` is `True`, this function blocks
+        until there are events to be read from the display fd. Events are read
+        and queued on the appropriate event queues. Finally, events on the
+        default event queue are dispatched.
 
         .. note::
 
             It is not possible to check if there are events on the queue or
-            not. For dispatching default queue events without blocking, see
-            :func:`Display.dispatch_pending`.
-
-        .. seealso::
-
-            :meth:`Display.dispatch_pending()`,
-            :meth:`Display.dispatch_queue()`
+            not.
         """
-        return lib.wl_display_dispatch(self._ptr)
+        if block:
+            if queue is None:
+                ret = lib.wl_display_dispatch(self._ptr)
+            else:
+                ret = lib.wl_display_dispatch_queue(self._ptr, queue._ptr)
+        else:
+            if queue is None:
+                ret = lib.wl_display_dispatch_pending(self._ptr)
+            else:
+                ret = lib.wl_display_dispatch_queue_pending(self._ptr, queue._ptr)
+
+        if ret == -1:
+            err = lib.wl_display_get_error(self._ptr)
+            raise RuntimeError("Failed with error: {}".format(err))
+
+        return ret
 
     @ensure_valid
-    def dispatch_pending(self) -> int:
-        """Dispatch default queue events without reading from the display fd
+    def roundtrip(self, *, queue: EventQueue = None) -> int:
+        """Block until all pending request are processed by the server
 
-        This function dispatches events on the main event queue. It does not
-        attempt to read the display fd and simply returns zero if the main
-        queue is empty, i.e., it doesn't block.
+        This function blocks until the server has processed all currently
+        issued requests by sending a request to the display server and waiting
+        for a reply before returning.
 
-        This is necessary when a client's main loop wakes up on some fd other
-        than the display fd (network socket, timer fd, etc) and calls
-        :meth:`Display.dispatch_queue()` from that callback. This may queue up
-        events in other queues while reading all data from the display fd.
-        When the main loop returns from the handler, the display fd
-        no longer has data, causing a call to poll (or similar functions) to
-        block indefinitely, even though there are events ready to dispatch.
-
-        To proper integrate the wayland display fd into a main loop, the
-        client should always call :meth:`Display.dispatch_pending()` and then
-        :meth:`Display.flush()` prior to going back to sleep. At that point,
-        the fd typically doesn't have data so attempting I/O could block, but
-        events queued up on the default queue should be dispatched.
-
-        A real-world example is a main loop that wakes up on a timerfd (or a
-        sound card fd becoming writable, for example in a video player), which
-        then triggers GL rendering and eventually eglSwapBuffers().
-        eglSwapBuffers() may call :meth:`Display.dispatch_queue()` if it didn't
-        receive the frame event for the previous frame, and as such queue
-        events in the default queue.
-
-        :returns: The number of dispatched events or -1 on failure
-
-        .. seealso::
-
-            :meth:`Display.dispatch()`, :meth:`Display.dispatch_queue()`,
-            :meth:`Display.flush()`
-        """
-        return lib.wl_display_dispatch_pending(self._ptr)
-
-    @ensure_valid
-    def dispatch_queue(self, queue: EventQueue) -> int:
-        """Dispatch events in an event queue
-
-        Dispatch all incoming events for objects assigned to the given event
-        queue. On failure -1 is returned and errno set appropriately.
-
-        The behaviour of this function is exactly the same as the behaviour of
-        :meth:`Display.dispatch()`, but it dispatches events on given queue,
-        not on the default queue.
-
-        This function blocks if there are no events to dispatch (if there are,
-        it only dispatches these events and returns immediately).  When this
-        function returns after blocking, it means that it read events from
-        display's fd and queued them to appropriate queues.  If among the
-        incoming events were some events assigned to the given queue, they are
-        dispatched by this moment.
+        This function uses wl_display_dispatch_queue() internally. It is not
+        allowed to call this function while the thread is being prepared for
+        reading events, and doing so will cause a dead lock.
 
         .. note::
 
-            Since Wayland 1.5 the display has an extra queue for its own events
-            (i.e. delete_id). This queue is dispatched always, no matter what
-            queue we passed as an argument to this function.  That means that
-            this function can return non-0 value even when it haven't
-            dispatched any event for the given queue.
+            This function may dispatch other events being received on the
+            default queue.
 
-        This function has the same constrains for using in multi-threaded apps
-        as :meth:`Display.dispatch()`.
-
-        :param queue: The event queue to dispatch
+        :param queue: The queue on which to run the roundtrip, if not given,
+                      uses the default queue.
         :type queue: :class:`~pywayland.client.EventQueue`
         :returns: The number of dispatched events on success or -1 on failure
-
-        .. seealso::
-
-            :meth:`Display.dispatch()`, :meth:`Display.dispatch_pending()`
         """
-        return lib.wl_display_dispatch_queue(self._ptr, queue._ptr)
+        if queue is None:
+            return lib.wl_display_roundtrip(self._ptr)
+        else:
+            return lib.wl_display_roundtrip_queue(self._ptr, queue._ptr)
 
     @ensure_valid
     def flush(self) -> int:
@@ -255,50 +216,3 @@ class Display(WlDisplay.proxy_class):  # type: ignore
         descriptor to wait for it to become writable again.
         """
         return lib.wl_display_flush(self._ptr)
-
-    @ensure_valid
-    def roundtrip(self) -> int:
-        """Block until all pending request are processed by the server
-
-        This function blocks until the server has processed all currently
-        issued requests by sending a request to the display server and waiting
-        for a reply before returning.
-
-        This function uses wl_display_dispatch_queue() internally. It is not
-        allowed to call this function while the thread is being prepared for
-        reading events, and doing so will cause a dead lock.
-
-        .. note::
-
-            This function may dispatch other events being received on the
-            default queue.
-
-        :returns: The number of dispatched events on success or -1 on failure
-        """
-        return lib.wl_display_roundtrip(self._ptr)
-
-    @ensure_valid
-    def roundtrip_queue(self, queue) -> int:
-        """Block until all pending request are processed by the server
-
-        This function blocks until the server has processed all currently
-        issued requests by sending a request to the display server and waiting
-        for a reply before returning.
-
-        This function uses wl_display_dispatch_queue() internally. It is not
-        allowed to call this function while the thread is being prepared for
-        reading events, and doing so will cause a dead lock.
-
-        .. note::
-
-            This function may dispatch other events being received on the given queue.
-
-        .. seealso::
-
-            :meth:`Display.roundtrip()`
-
-        :param queue: The queue on which to run the roundtrip
-        :type queue: :class:`~pywayland.client.EventQueue`
-        :returns: The number of dispatched events on success or -1 on failure
-        """
-        return lib.wl_display_roundtrip_queue(self._ptr, queue._ptr)
--- pywayland/ffi_build.py
+++ pywayland/ffi_build.py
@@ -113,13 +113,19 @@ struct wl_display* wl_display_connect_to_fd(int fd);
 void wl_display_disconnect(struct wl_display *display);
 int wl_display_get_fd(struct wl_display *display);
 int wl_display_dispatch(struct wl_display *display);
+int wl_display_dispatch_pending(struct wl_display *display);
 int wl_display_dispatch_queue(struct wl_display *display,
                               struct wl_event_queue *queue);
-int wl_display_dispatch_pending(struct wl_display *display);
-int wl_display_flush(struct wl_display *display);
+int wl_display_dispatch_queue_pending(struct wl_display *display,
+                                      struct wl_event_queue *queue);
 int wl_display_roundtrip(struct wl_display *display);
 int wl_display_roundtrip_queue(struct wl_display *display,
                                struct wl_event_queue *queue);
+int wl_display_read_events(struct wl_display *display);
+int wl_display_prepare_read(struct wl_display *display);
+int wl_display_prepare_read_queue(struct wl_display *display,
+                                  struct wl_event_queue *queue);
+int wl_display_flush(struct wl_display *display);
 struct wl_event_queue *wl_display_create_queue(struct wl_display *display);
 """
 
