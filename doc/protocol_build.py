102c07c9b6844c68cc14b6051f0fcde537c1734b
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-12 20:44:10 +0000

    Adding tarfile member sanitization to extractall()

--- doc/protocol_build.py
+++ doc/protocol_build.py
@@ -26,7 +26,26 @@ def protocols_build(output_dir):
     protocol_dest = "wayland-protocols-{}".format(protocols_version)
     urllib.request.urlretrieve(protocols_source, protocol_dest + ".tar.xz")
     with tarfile.open(protocol_dest + ".tar.xz") as f:
-        f.extractall()
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(f)
 
     # walk the directory and generate all the protocols
     protocol_files = [wayland_file] + sorted(
